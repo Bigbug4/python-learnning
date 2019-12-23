c6117530f8863898ee96a6fe0b2638943f541b34
Bigbug4 <1696648046@qq.com>
2019-12-23 09:59:49 +0800

    Signed-off-by: Bigbug4 <1696648046@qq.com>

--- pynput/mouse_trick.py
+++ pynput/mouse_trick.py
@@ -17,13 +17,13 @@ m = Controller()
 
 # mouse left click
 
-def left_trick(click=2,interval=10):
+def left_trick(click=2,interval=1):
 
      with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
        # listener. join()
 
         #for i in range(times):
-		while True:
+        while True:
             #Double click
             m.click(Button.left, click)
             print('The current pointer position is {0}'.format(m.position))
@@ -31,6 +31,13 @@ def left_trick(click=2,interval=10):
 
             if not listener.is_alive():
                 break
+            
+        print('Exited!')
 
 if __name__ == '__main__':
+
+#    path=r'D:\PyCharm Community Edition 2017.2.1\python-learnning\pynput'
+#    import os
+#    os.chdir(path)
+    
     left_trick()
